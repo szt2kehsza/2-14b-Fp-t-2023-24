d48e4ed607937d11dcc6a7b74cb5257585fe64a6
szt2kehsza <szt2kehsza@vasvari.org>
2023-09-21 10:46:56 +0200

    javitva

--- cssvaltozok3/cssvaltozok3.js
+++ cssvaltozok3/cssvaltozok3.js
@@ -34,15 +34,3 @@ function getDivIdByNumber(divNumber) {
     }
 }
 
-function Onchange(){
-    const magassag = parseInt(document.getElementById("magassag").value);
-    const szelesseg = parseInt(document.getElementById("szelesseg").value);
-    
-    const divs = document.getElementsByClassName('items');
-    
-    for (let i = 0; i < divs.length; i++) {
-        divs[i].style.height = magassag + 'px';
-        divs[i].style.width = szelesseg + 'px';
-    }
-
-}
