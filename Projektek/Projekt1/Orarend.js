eaeeeddbb40958f81f848d1afaaa5bd5b14defbe
szt2kehsza <115459926+szt2kehsza@users.noreply.github.com>
2023-10-04 18:21:59 +0200

    "kész"

--- Projektek/Projekt1/Orarend.js
+++ Projektek/Projekt1/Orarend.js
@@ -6,34 +6,35 @@ function DarkModeChange() {
     if (darkMode) {
         document.documentElement.style.setProperty('--kartyakhatterszine', 'white');
         document.documentElement.style.setProperty('--szovegszine', 'black');
-        document.documentElement.style.setProperty('--atlatszohattervilagosm', 'rgba(255, 255, 255, 0.5)');
+        document.documentElement.style.setProperty('--atlatszohattersotetm', 'rgba(255, 255, 255, 0.5)');
         button.style.backgroundImage = "url(cartoon-moon-icon.png)";
         darkMode = false; 
     } else {
         document.documentElement.style.setProperty('--kartyakhatterszine', 'black');
         document.documentElement.style.setProperty('--szovegszine', 'white');
-        document.documentElement.style.setProperty('--atlatszohattervilagosm', 'rgba(0, 0, 0, 0.5)');
+        document.documentElement.style.setProperty('--atlatszohattersotetm', 'rgba(0, 0, 0, 0.5)');
         button.style.backgroundImage = "url(Sun.png)";
         darkMode = true; 
         
     }
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.getElementById("menuIcon");
     const menu = document.getElementById("menu");
     const closeBtn = document.getElementById("closeBtn");
+    const cim = document.getElementById("cim");
 
     menuIcon.addEventListener("click", function() {
         menu.style.display = "block";
+        cim.style.display = "none"; //megkerdeznil, hogy jo-e!
     });
 
     closeBtn.addEventListener("click", function() {
         menu.style.display = "none";
+        cim.style.display = "block"; //megkerdeznil, hogy jo-e!
     });
 });
-
 
 const root = document.querySelector(':root')
 
